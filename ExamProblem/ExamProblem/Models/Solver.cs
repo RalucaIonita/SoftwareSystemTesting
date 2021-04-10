8d8de601a4d5561a0528842b0ef8fc469f30cd26
RalucaIonita <raluca.ionita04@gmail.com>
2021-04-10 18:40:04 +0300

    [Fix]

--- ExamProblem/ExamProblem/Models/Solver.cs
+++ ExamProblem/ExamProblem/Models/Solver.cs
@@ -52,7 +52,6 @@ namespace ExamProblem.Models
                     foreach (var third in map.Points)
                     {
                         var canForm = first.CanFormTriangle(second, third);
-                        // Console.WriteLine(canForm);
                         if(canForm)
                             triangles.Add(new Triangle(first, second, third));
                     }
@@ -61,49 +60,16 @@ namespace ExamProblem.Models
             }
 
 
-
-            //
-            // for(var i = 0; i < map.Points.Count - 2; i++)
-            //     for(var j = i + 1; j < map.Points.Count - 1; j++)
-            //         for (var k = j + 1; k < map.Points.Count; k++)
-            //         {
-            //             var a = map.Points[i];
-            //             var b = map.Points[j];
-            //             var c = map.Points[k];
-            //             if (a.CanFormTriangle(b, c))
-            //                 triangles.Add(new Triangle(a, b, c));
-            //         }
             return triangles;
         }
 
         public static bool AreTranslated(Triangle t1, Triangle t2)
         {
             var origin = new Point(0, 0);
-            // Console.WriteLine("\n\n\n\n\nTriangles:");
-            // Console.WriteLine(t1.A.X + " " + t2.A.Y);
-            // Console.WriteLine(t1.B.X + " " + t2.B.Y);
-            // Console.WriteLine(t1.B.X + " " + t2.B.Y);
-            // Console.WriteLine("\n");
-            //
-            // Console.WriteLine("Triangles:");
-            // Console.WriteLine(t2.A.X + " " + t2.A.Y);
-            // Console.WriteLine(t2.B.X + " " + t2.B.Y);
-            // Console.WriteLine(t2.B.X + " " + t2.B.Y);
-            // Console.WriteLine("\n");
 
             var dA = t1.A - t2.A;
             var dB = t1.B - t2.B;
             var dC = t1.C - t2.C;
-
-
-            //A1-A2; B1-B2; C1-C2
-            // dA.X = A1.X - A2.X;
-            // dB.X = B1.X - B2.X;
-            // dC.X = C1.X - C2.X;
-            //
-            // dA.Y = A1.Y - A2.Y;
-            // dB.Y = B1.Y - B2.Y;
-            // dC.Y = C1.Y - C2.Y;
 
 
             Console.WriteLine("A: " + dA.X + " " + dA.Y);
